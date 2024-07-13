092868b30ad22c1f30488e42000fcf1ebd144b4f
KrishKoria <krishkoria2004@gmail.com>
2024-07-14 01:43:13 +0530

    added auth with clerk

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,9 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { MobileMenu } from "./mobilemenu";
+import { ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 
 export default function Navbar() {
   return (
@@ -48,6 +51,37 @@ export default function Navbar() {
       </div>
       {/* Right */}
       <div className="flex w-[30%] items-center justify-end gap-4 xl:gap-8">
+        <ClerkLoading>
+          <div
+            className="text-surface inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+            role="status"
+          >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <div className="cursor-pointer">
+              <Image src={"/people.png"} alt="" width={20} height={20} />
+            </div>
+            <div className="cursor-pointer">
+              <Image src={"/messages.png"} alt="" width={20} height={20} />
+            </div>
+            <div className="cursor-pointer">
+              <Image src={"/notifications.png"} alt="" width={20} height={20} />
+            </div>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <div className="flex items-center gap-2 text-sm">
+              <Image src={"/login.png"} alt="" width={20} height={20} />
+              <Link href={"/sign-in"}>Login/Register</Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
+
         <MobileMenu />
       </div>
     </div>
